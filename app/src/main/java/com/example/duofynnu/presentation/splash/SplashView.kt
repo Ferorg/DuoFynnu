235d5048002facc5e00e83ef33a42cf44e583b41
anton <antonefrim@mail.ru>
2024-05-12 22:06:28 +0300

    commit 3:add temporary logo

--- app/src/main/java/com/example/duofynnu/presentation/splash/SplashView.kt
+++ app/src/main/java/com/example/duofynnu/presentation/splash/SplashView.kt
@@ -1,4 +1,21 @@
 package com.example.duofynnu.presentation.splash
 
-class SplashView {
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.duofynnu.R
+
+class SplashView : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash_view)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
 }
