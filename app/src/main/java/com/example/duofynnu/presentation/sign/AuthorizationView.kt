cd03bd2ff0f746166848143ab109f545c6bcbe14
anton <antonefrim@mail.ru>
2024-05-12 23:16:45 +0300

    commit 7:create navigation file for AuthorizationView

--- app/src/main/java/com/example/duofynnu/presentation/sign/AuthorizationView.kt
+++ app/src/main/java/com/example/duofynnu/presentation/sign/AuthorizationView.kt
@@ -1,21 +1,14 @@
 package com.example.duofynnu.presentation.sign
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.example.duofynnu.R
+import com.example.duofynnu.databinding.ActivityAuthorizationViewBinding
 
 class AuthorizationView : AppCompatActivity() {
+    private lateinit var authorizationViewBinding: ActivityAuthorizationViewBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_authorization_view)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        authorizationViewBinding=ActivityAuthorizationViewBinding.inflate(layoutInflater)
+        setContentView(authorizationViewBinding.root)
     }
 }
